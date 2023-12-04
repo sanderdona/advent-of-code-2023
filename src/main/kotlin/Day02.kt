fc6fd348f51f94d41d57c492f35ffa1107db8f65
sanderdona <spdona@gmail.com>
2023-12-04 20:18:12 +0100

    Day 2, part 2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -7,17 +7,37 @@ class Day02 {
         return inputs.sumOf { input -> getPossibleId(input) }
     }
 
+    fun executePartTwo(inputs: List<String>): Int {
+        return inputs.sumOf { input -> sumOfFewestCubesNeeded(input) }
+    }
+
     private fun getPossibleId(input: String): Int {
         val gameId = getGameId(input)
         return if (enoughCubesForInput(input)) gameId else 0
     }
 
+    private fun sumOfFewestCubesNeeded(input: String): Int {
+        val minimumCubesNeeded = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+        val resultSequence = regex.findAll(input)
+
+        resultSequence.forEach {
+            val color = it.value.trim().split(" ")[1]
+            val number = it.value.trim().split(" ")[0].toInt()
+            val minimumNeeded = minimumCubesNeeded[color] ?: 0
+            if (number > minimumNeeded) minimumCubesNeeded[color] = number
+        }
+
+        return minimumCubesNeeded.values.fold(1) { accumulator, value ->
+            accumulator * value
+        }
+    }
+
     private fun enoughCubesForInput(input: String): Boolean {
         val resultSequence = regex.findAll(input)
 
-        return resultSequence.filter { item ->
-            val color = item.value.trim().split(" ")[1]
-            val number = item.value.trim().split(" ")[0].toInt()
+        return resultSequence.filter {
+            val color = it.value.trim().split(" ")[1]
+            val number = it.value.trim().split(" ")[0].toInt()
             val availableCubes = cubes[color] ?: 0
             availableCubes < number
         }.toList().isEmpty()
--- src/test/kotlin/Day02Test.kt
+++ src/test/kotlin/Day02Test.kt
@@ -15,10 +15,26 @@ class Day02Test {
     }
 
     @Test
+    fun examplePartTwoTest() {
+        val day02 = Day02()
+        val output = day02.executePartTwo(fileUtil.getRows())
+
+        assertEquals(2286, output)
+    }
+
+    @Test
     fun test() {
         val day02 = Day02()
         val output = day02.execute(fileUtil.getRows())
 
-        assertEquals(1629, output) // 2402  // 2913
+        assertEquals(2913, output)
+    }
+
+    @Test
+    fun testPartTwo() {
+        val day02 = Day02()
+        val output = day02.executePartTwo(fileUtil.getRows())
+
+        assertEquals(55593, output)
     }
 }
